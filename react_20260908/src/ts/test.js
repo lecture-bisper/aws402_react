021a6da591532a8d13999608577e8986c6c75921
lecture-bisper <bisper@outlook.kr>
2026-09-09 17:46:19 +0900

    React - typescript : 타입스크립트란?

---
+++ react_20260908/src/ts/test.js
@@ -0,0 +1,12 @@
+"use strict";
+//  File :  test.ts
+//  User :  it
+//  Date :  2026-09-08
+//  Time :  오전 9:16
+//  Desc :  
+//  typescript : javascript 의 데이터 타입에 대한 부족한 부분을 추가하여 새로 만든 javascript 의 superset 언어
+//  typescript 는 자체적으로 동작하는 것이 아니라 실제 동작 시 컴파일을 통해서 javascript 로 변경되어 사용됨
+let data = 10;
+console.log(`원본 데이터 : ${data}`);
+data = 20;
+console.log(`수정된 데이터 : ${data}`);
