45daad15037bb7cdd8f58fd680c35b68afce902a
lecture-bisper <bisper@outlook.kr>
2026-09-10 17:54:01 +0900

    React - 부트스트랩 사용하기

---
+++ react_20260910/src/components/BootstrapCDN.tsx
@@ -0,0 +1,26 @@
+//  File :  BootstrapCDN.tsx
+//  User :  it
+//  Date :  2026-09-10
+//  Time :  오후 2:55
+//  Desc :  
+
+function BootstrapCDN() {
+  return (
+    <>
+      <button>일반 버튼</button><br/><br/>
+      <button className={'btn btn-primary'}>CDN 으로 부트스트랩 적용한 버튼</button>
+    </>
+  );
+}
+
+export default BootstrapCDN;
+
+
+
+
+
+
+
+
+
+
---
+++ react_20260910/src/components/BootstrapReact.tsx
@@ -0,0 +1,28 @@
+//  File :  BootstrapReact.tsx
+//  User :  it
+//  Date :  2026-09-10
+//  Time :  오후 3:00
+//  Desc :  
+
+import {Button} from "react-bootstrap";
+
+function BootstrapReact() {
+  return (
+    <>
+      <button>일반 버튼</button>
+      <Button variant={'primary'}>React Bootstrap 으로 만든 버튼</Button>
+    </>
+  );
+}
+
+export default BootstrapReact;
+
+
+
+
+
+
+
+
+
+
